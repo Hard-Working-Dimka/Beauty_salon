d2e22f26bbf37ae57c9a923b89d209a87d907063
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2025-06-27 21:20:52 +0300

    add tables from Salons model to admin panel and add Salons into setting intalled apps

--- Beauty_salon/settings.py
+++ Beauty_salon/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'phonenumber_field',
-
+    'Salons',
     'CustomUser.apps.CustomuserConfig',
 ]
 
--- Salons/admin.py
+++ Salons/admin.py
@@ -1,3 +1,75 @@
 from django.contrib import admin
+from .models import (
+    ServiceType,
+    BeautyService,
+    Salon,
+    BeautyServiceInSalon,
+    Review,
+    Specialist,
+    EXTENSIONpromo,
+    Appointment,
+    SpecialistSkills,
+    SpecialistV2,
+    WorkScheduleV2,
+    EXTENSIONreview
+)
 
-# Register your models here.
+
+@admin.register(ServiceType)
+class ServiceTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BeautyService)
+class BeautyServiceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Salon)
+class SalonAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BeautyServiceInSalon)
+class BeautyServiceInSalonAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Specialist)
+class SpecialistAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(EXTENSIONpromo)
+class EXTENSIONpromoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(SpecialistSkills)
+class SpecialistSkillsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(SpecialistV2)
+class SpecialistV2Admin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(WorkScheduleV2)
+class WorkScheduleV2Admin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(EXTENSIONreview)
+class EXTENSIONreviewAdmin(admin.ModelAdmin):
+    pass
