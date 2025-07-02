6b8adffbe4f702826a3e4e2c155f1d6f0572a14f
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2025-07-02 11:32:15 +0300

    add readme and env

--- Beauty_salon/settings.py
+++ Beauty_salon/settings.py
@@ -1,5 +1,9 @@
 import os
 from pathlib import Path
+from environs import Env
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fh2tsi)f+k&i4-jdp+!g*gx*e(_qix6v(!yfqi2qf4i6kwg2y0'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
